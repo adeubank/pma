02e98a8f0a0cb988a0fc21c2207e3c5f98dc11eb
Allen Eubank <adeubank@gmail.com>
2017-01-22 17:18:15 -0800

    Remove serve command

--- pma.sh
+++ pma.sh
@@ -6,13 +6,3 @@ curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.6.4/phpMyAdmin-4.6.4-english.
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
 rm phpmyadmin.tar.gz
-
-CMD=/vagrant/scripts/serve-laravel.sh
-if [ ! -f $CMD ]; then
-    # fallback for older versions
-    CMD=/vagrant/scripts/serve.sh
-fi
-
-sudo bash $CMD phpmyadmin.app $(pwd)/phpmyadmin
-
-sudo service nginx reload
